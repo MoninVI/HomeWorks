804d305e79ff9058ad4e98f0661a56ed2af1cec7
MoninVI <monin.vi.96@gmail.com>
2021-11-16 16:21:19 +0300

    Выполеннное первое ДЗ

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-let name = 'Вячеслав';
+function consoleRec(a, b) {
+    console.log(a[b]);
+    b += 1
+    if (b < a.length) {
+        consoleRec(a, b);
+    }
+};
+
+consoleRec(['я', 'умею', 'писать', 'рекурсивные', 'функции'], 0);
